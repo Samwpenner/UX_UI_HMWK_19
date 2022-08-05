e226fc767f0c933c337ea5201b64b8cd381d57a4
Sam Penner <60679136+Samwpenner@users.noreply.github.com>
2022-08-05 15:38:46 -0700

    HMWK 19
    Easy peezy

---
+++ js/style.js
@@ -0,0 +1,7 @@
+ScrollReveal({ duration: 1000})
+
+ScrollReveal().reveal('.heroImg', { delay: 500});
+
+ScrollReveal().reveal('.mySkills', { delay: 500});
+
+ScrollReveal().reveal('.myWork', { delay: 500});
